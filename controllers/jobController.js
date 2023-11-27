ea204e865b11ce4cd8716d1b46ef612a9a275db5
Meer Uzair <meer.uxair007@gmail.com>
2023-11-27 11:13:30 +0530

    added job query

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,57 +1,56 @@
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
-//import day from "dayjs";
+import day from "dayjs";
 
 export const getAllJobs = async (req, res) => {
-  //   const { search, jobStatus, jobType, sort } = req.query;
+  const { search, jobStatus, jobType, sort } = req.query;
 
-  //   const queryObject = {
-  //     createdBy: req.user.userId,
-  //   };
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
 
-  //   if (search) {
-  //     queryObject.$or = [
-  //       { position: { $regex: search, $options: "i" } },
-  //       { company: { $regex: search, $options: "i" } },
-  //     ];
-  //   }
+  if (search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: "i" } },
+      { company: { $regex: search, $options: "i" } },
+    ];
+  }
 
-  //   if (jobStatus && jobStatus !== "all") {
-  //     queryObject.jobStatus = jobStatus;
-  //   }
-  //   if (jobType && jobType !== "all") {
-  //     queryObject.jobType = jobType;
-  //   }
+  if (jobStatus && jobStatus !== "all") {
+    queryObject.jobStatus = jobStatus;
+  }
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
 
-  //   const sortOptions = {
-  //     newest: "-createdAt",
-  //     oldest: "createdAt",
-  //     "a-z": "position",
-  //     "z-a": "-position",
-  //   };
+  const sortOptions = {
+    newest: "-createdAt",
+    oldest: "createdAt",
+    "a-z": "position",
+    "z-a": "-position",
+  };
 
-  //   const sortKey = sortOptions[sort] || sortOptions.newest;
+  const sortKey = sortOptions[sort] || sortOptions.newest;
 
-  //   // setup pagination
+  // setup pagination
 
-  //   const page = Number(req.query.page) || 1;
-  //   const limit = Number(req.query.limit) || 10;
-  //   const skip = (page - 1) * limit;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-  //   const jobs = await Job.find(queryObject)
-  //     .sort(sortKey)
-  //     .skip(skip)
-  //     .limit(limit);
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
 
-  //   const totalJobs = await Job.countDocuments(queryObject);
-  //   const numOfPages = Math.ceil(totalJobs / limit);
-  //   res
-  //     .status(StatusCodes.OK)
-  //     .json({ totalJobs, numOfPages, currentPage: page, jobs });
-  const jobs = await Job.find({ createdBy: req.user.userId });
-  res.status(StatusCodes.OK).json({ jobs });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res
+    .status(StatusCodes.OK)
+    .json({ totalJobs, numOfPages, currentPage: page, jobs });
+  // const jobs = await Job.find({ createdBy: req.user.userId });
 };
 
 export const createJob = async (req, res) => {
@@ -91,51 +90,50 @@ export const deleteJob = async (req, res) => {
 };
 
 export const showStats = async (req, res) => {
-  //   let stats = await Job.aggregate([
-  //     { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
-  //     { $group: { _id: '$jobStatus', count: { $sum: 1 } } },
-  //   ]);
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$jobStatus", count: { $sum: 1 } } },
+  ]);
 
-  //   stats = stats.reduce((acc, curr) => {
-  //     const { _id: title, count } = curr;
-  //     acc[title] = count;
-  //     return acc;
-  //   }, {});
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
 
-  //   const defaultStats = {
-  //     pending: stats.pending || 0,
-  //     interview: stats.interview || 0,
-  //     declined: stats.declined || 0,
-  //   };
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
 
-  //   let monthlyApplications = await Job.aggregate([
-  //     { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
-  //     {
-  //       $group: {
-  //         _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
-  //         count: { $sum: 1 },
-  //       },
-  //     },
-  //     { $sort: { '_id.year': -1, '_id.month': -1 } },
-  //     { $limit: 6 },
-  //   ]);
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
 
-  //   monthlyApplications = monthlyApplications
-  //     .map((item) => {
-  //       const {
-  //         _id: { year, month },
-  //         count,
-  //       } = item;
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
 
-  //       const date = day()
-  //         .month(month - 1)
-  //         .year(year)
-  //         .format('MMM YY');
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format("MMM YY");
 
-  //       return { date, count };
-  //     })
-  //     .reverse();
+      return { date, count };
+    })
+    .reverse();
 
-  //   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
-  res.status(StatusCodes.OK).json({ msg: "working" });
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -16,12 +16,15 @@ import {
 } from "../middleware/validationMiddleware.js";
 import { checkForTestUser } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getAllJobs).post(checkForTestUser,validateJobInput, createJob);
-
+router
+  .route("/")
+  .get(getAllJobs)
+  .post(checkForTestUser, validateJobInput, createJob);
+router.route("/stats").get(showStats);
 router
   .route("/:id")
   .get(validateIdParam, getJob)
-  .patch(checkForTestUser,validateJobInput, validateIdParam, updateJob)
-  .delete(checkForTestUser,validateIdParam, deleteJob);
+  .patch(checkForTestUser, validateJobInput, validateIdParam, updateJob)
+  .delete(checkForTestUser, validateIdParam, deleteJob);
 
 export default router;
